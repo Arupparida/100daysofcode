cf5991d063c3b248d32b867ceeb54e6a67af8589
Arup Parida <57106944+Arupparida@users.noreply.github.com>
2022-03-12 16:25:54 +0530

    Day 5 : simple stdin n stdout

---
+++ solution_hackerrank.java
@@ -0,0 +1,31 @@
+//link:https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true 
+
+//One popular way to read input from stdin is by using the Scanner class and specifying the Input Stream as System.in. 
+
+
+
+
+
+
+
+import java.util.*;
+
+public class Solution_hackerrank {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+       
+        int b = scan.nextInt();
+        //The nextInt(radix) method of java. util. Scanner class scans the next token of the input as a Int. 
+       
+        int c = scan.nextInt();
+        
+
+        System.out.println(a);
+       
+        System.out.println(b);
+        e
+        System.out.println(c);
+    }
+}
