754e43895262417f0d9e790833126725c021de73
Arup Parida <57106944+Arupparida@users.noreply.github.com>
2022-03-13 21:52:37 +0530

    Create MoneyTransferService.java

---
+++ MoneyTransferService.java
@@ -0,0 +1,67 @@
+package com.company;
+
+public class MoneyTransferService {
+
+
+
+
+//
+//    double[] exchangeRates = new double[] {63.0, 3.0, 3.0, 595.5, 18.0, 107.0, 2.0};
+//
+//
+//
+//    void setExchangeRates(double[] rates) {
+//
+//        exchangeRates = rates;
+//
+//    }
+//
+//    void updateExchangeRate(int countryIndex, double newVal) {
+//
+//        exchangeRates[countryIndex] = newVal;
+//
+//    }
+//
+//
+//
+//    double getExchangeRate(int countryIndex) {
+//
+//        return exchangeRates[countryIndex];}
+
+//
+
+    Currency_converter2 cc2 = new Currency_converter2();
+
+
+
+    double computeTransfer_Amount(int countryIndex, double amount) {
+
+        return cc2.computeTransfer_Amount(countryIndex,amount);
+
+    }
+
+    double computeTransferFee(int countryIndex, double amount){
+        double transferFee = 0.02 * computeTransfer_Amount(countryIndex,amount);
+        return transferFee;
+    }
+
+
+
+        public static void main(String[] args) {
+
+            MoneyTransferService transferService = new MoneyTransferService();
+
+            double transferAmount = transferService.computeTransfer_Amount(0, 1000);
+
+            double transferFee = transferService.computeTransferFee(0, 1000);
+
+            System.out.println("Transfer Amount: " + transferAmount);
+
+            System.out.println("Transfer Fee: " + transferFee);
+
+        }
+
+        //public static void main(String[] args)
+
+
+}
