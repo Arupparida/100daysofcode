582430088dc5210a6816b752b2fe825ae3ae7953
Arup Parida <57106944+Arupparida@users.noreply.github.com>
2022-07-01 23:34:01 +0530

    Day _9_continue

---
+++ arraydsa1L_C.java
@@ -0,0 +1,39 @@
+/*
+Contains Duplicate Question - leetcode (Easy level) #DSA begins
+Problem statemnet link - https://leetcode.com/problems/contains-duplicate/
+My Possible solutions 
+ class Solution {
+     public boolean containsDuplicate(int[] nums) {
+         if(nums ==null || nums.length<2)
+             return false;
+         Arrays.sort(nums);
+
+        
+         for (int i = 1;i<nums.length;i++)
+             
+             if (nums[i-1] == nums[i])
+                 return true;
+           
+        
+        return false;
+     }
+     
+ }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         if(nums == null || nums.length<2)
+//             return false;
+//         Arrays.sort(nums);
+//         for (int i = 1;i<nums.length;i++)
+//             
+//             if (nums[i-1] == nums[i])
+//                 return true;
+            
+        
+//         return false;
+//     }
+//     
+// }
+
+*/
